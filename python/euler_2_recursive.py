64257448a57f523160037c52fa0ef78812238656
Haakon Borch <haakonborch@gmail.com>
2018-07-26 09:57:20 +0200

    Setting the recursion limit higher simple results in segfault. Indicates recursion is inherently flawed approach. Or that my approach is inherently flawed. Either or.

--- python/euler_2_recursive.py
+++ python/euler_2_recursive.py
@@ -1,4 +1,8 @@
 #Max recursion depth exceeded. Could set it higher, but might get stack overflow. Recursion is not optimised for solving this problem. 
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+
 memo = {0:0, 1:1}
 def fib(num):
     if not num in memo:
